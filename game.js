b49ed01937b9461dc170b4cac8d4b395a8aa9f19
shubhamatkal <shubhamatkal333@gmail.com>
2023-10-21 21:16:56 +0530

    check answers

--- game.js
+++ game.js
@@ -19,14 +19,15 @@ $(".btn").click(function(){
 var userChosenColour = $(this).attr("id");
 userClickedPattern.push(userChosenColour);
 playSound(userChosenColour);
-
 animatePress(userChosenColour);
+checkAnswers(userClickedPattern.length-1)
 })
 
 
 
 
 function nextSequence(){
+userClickedPattern = [];
 level++;
 $("#level-title").text("Level "+level);
 var randomNumber = Math.floor(Math.random() * 4);
@@ -56,7 +57,27 @@ setTimeout(function () {
 }, 100);
 }
 
+function checkAnswers(currentLevel){
+if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
 
+      console.log("success");
+
+      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamePattern.length){
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+
+      console.log("wrong");
+
+    }
+
+}
 
 
 
